fa58f78a9ceaf1855fe5c2b997312f5bec2fb14c
Ayşe Nur Şahin <57765545+aysenursahin351@users.noreply.github.com>
2023-06-10 15:54:33 +0300

    Update ProductManager.java

--- ProductManager.java
+++ ProductManager.java
@@ -103,25 +103,7 @@ public class ProductManager {
     System.out.println("Total Amount: " + order.getTotalAmount());
   }
 
-  /*public List<Product> getActiveProductsSortedByPrice() {
-    // ProductStatus'ü ACTIVE olan ürünleri fiyatlarına göre sıralayıp döndüren metodu yazın
-    return null;
-  }
-
-  public double calculateAveragePriceInCategory(String category) {
-    // String olarak verilen category'e ait olan ürünlerin fiyatlarının ortalamasını yoksa 0.0 döndüren metodu yazın
-    // tip: OptionalDouble kullanımını inceleyin.
-    return 0.0;
-  }
-
-  public Map<String, Double> getCategoryPriceSum() {
-    // category'lere göre gruplayıp, her bir kategoride bulunan ürünlerin toplam fiyatını stream ile hesaplayıp
-    // döndüren metodu yazın
-    // örn:
-    // category-1 105.2
-    // category-2 45.0
-    return null;
-  }*/
+ ////Added 3 funtions
   public List<Product> getActiveProductsSortedByPrice() {
 	  return products.values()
 	  .stream()
